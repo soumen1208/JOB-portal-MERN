31b82baf460c6b966d2d2d56c70c64d6a80f55bd
soumen1208 <soumenmahato4454@gmail.com>
2024-10-07 08:24:11 +0530

    v1

---
+++ frontend/src/utils/constant.js
@@ -0,0 +1,6 @@
+const url = "https://job-portal-backend-h2xq.onrender.com";
+
+export const USER_API_END_POINT = `${url}/api/v1/user`;
+export const JOB_API_END_POINT = `${url}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${url}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${url}/api/v1/company`;
